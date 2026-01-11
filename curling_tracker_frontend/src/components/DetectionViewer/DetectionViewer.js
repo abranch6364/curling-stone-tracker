bccc3d88920396b5c7c3f85e9115016b2e7abf4b
Andrew Branch <abranch6364@gmail.com>
2026-01-11 00:31:07 -0700

    Separated detection viewing into its own component

---
+++ curling_tracker_frontend/src/components/DetectionViewer/DetectionViewer.js
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { VStack, Heading, Box, Select, Portal, createListCollection } from "@chakra-ui/react";
+
+import ImageViewer from "../ImageViewer/ImageViewer";
+
+import { toIntPercent, findInsertionPoint } from "../../utility/CurlingStoneHelper";
+
+const DetectionViewer = ({ selectedTime, detections, detectionTimes }) => {
+  const [imageDimensions, setImageDimensions] = useState(null);
+
+  const [selectedCameraView, setSelectedCameraView] = useState(null);
+
+  //////////////////
+  //Helper Functions
+  //////////////////
+
+  const getDetectionsIndexForTime = (time) => {
+    if (!detectionTimes) {
+      return 0;
+    }
+    return Math.max(findInsertionPoint(detectionTimes, time) - 1, 0);
+  };
+
+  ///////////
+  //Callbacks
+  ///////////
+
+  const selectImage = () => {
+    if (detectionTimes && detections) {
+      const timeIndex = Math.max(findInsertionPoint(detectionTimes, selectedTime) - 1, 0);
+      if (timeIndex >= 0 && timeIndex < detections.length) {
+        return detections[timeIndex].images[selectedCameraView];
+      }
+    }
+
+    return "";
+  };
+
+  return (
+    <VStack>
+      <Heading as="h3" size="md">
+        Camera Views
+      </Heading>
+
+      <Select.Root
+        value={selectedCameraView ? [selectedCameraView] : []}
+        collection={createListCollection({
+          items: detections ? Object.keys(detections[getDetectionsIndexForTime(selectedTime)].images) : [],
+        })}
+        size="sm"
+        width="320px"
+        onValueChange={(details) => setSelectedCameraView(details.value[0])}
+      >
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Select Camera View" />
+          </Select.Trigger>
+        </Select.Control>
+        <Portal>
+          <Select.Positioner>
+            <Select.Content>
+              {detections &&
+                Object.keys(detections[getDetectionsIndexForTime(selectedTime)].images).map((key) => (
+                  <Select.Item item={key} key={key}>
+                    {key}
+                    <Select.ItemIndicator />
+                  </Select.Item>
+                ))}
+            </Select.Content>
+          </Select.Positioner>
+        </Portal>
+      </Select.Root>
+
+      <Box position="relative">
+        {detections &&
+          imageDimensions &&
+          selectedCameraView &&
+          detections[getDetectionsIndexForTime(selectedTime)].detections[selectedCameraView].map((detection) => (
+            <Box
+              position="absolute"
+              left={toIntPercent(detection.image_coordinates[0], imageDimensions.width) + "%"}
+              top={toIntPercent(detection.image_coordinates[1], imageDimensions.height) + "%"}
+              width={toIntPercent(detection.image_coordinates[2], imageDimensions.width) + "%"}
+              height={toIntPercent(detection.image_coordinates[3], imageDimensions.height) + "%"}
+              border={"1px solid " + detection.color}
+              opacity="1"
+              pointerEvents="none"
+              zLevel="1000"
+            ></Box>
+          ))}
+        {detections && (
+          <ImageViewer
+            file={selectImage()}
+            includeLoadButton={false}
+            onImageDimensionChange={setImageDimensions}
+            encodingType="data:image/png;base64,"
+          ></ImageViewer>
+        )}
+      </Box>
+    </VStack>
+  );
+};
+
+export default DetectionViewer;
--- curling_tracker_frontend/src/components/VideoDetect/VideoDetect.js
+++ curling_tracker_frontend/src/components/VideoDetect/VideoDetect.js
@@ -1,25 +1,14 @@
 import { useState } from "react";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
-import {
-  Button,
-  HStack,
-  VStack,
-  Heading,
-  RadioGroup,
-  Box,
-  Input,
-  Select,
-  Portal,
-  createListCollection,
-} from "@chakra-ui/react";
+import { Button, HStack, VStack, Heading, RadioGroup, Input } from "@chakra-ui/react";
 
 import CurlingSheetPlot from "../CurlingSheetPlot/CurlingSheetPlot";
 import AnimationSlider from "../AnimationSlider/AnimationSlider";
 import FetchDropdown from "../FetchDropdown/FetchDropdown";
 import TimeInput from "../TimeInput/TimeInput";
-import ImageViewer from "../ImageViewer/ImageViewer";
+import DetectionViewer from "../DetectionViewer/DetectionViewer";
 
-import { toIntPercent, findInsertionPoint, getStoneMinTime, getStoneMaxTime } from "../../utility/CurlingStoneHelper";
+import { findInsertionPoint, getStoneMinTime, getStoneMaxTime } from "../../utility/CurlingStoneHelper";
 
 const VideoDetect = () => {
   const [setupId, setSetupId] = useState("");
@@ -33,10 +22,7 @@ const VideoDetect = () => {
   const [detectionTimes, setDetectionTimes] = useState(null);
   const [detections, setDetections] = useState(null);
 
-  const [imageDimensions, setImageDimensions] = useState(null);
-
   const [sliderTime, setSliderTime] = useState(0);
-  const [selectedCameraView, setSelectedCameraView] = useState(null);
 
   const queryClient = useQueryClient();
 
@@ -53,13 +39,6 @@ const VideoDetect = () => {
       body: JSON.stringify(video_tracking_request),
     });
     return response.json();
-  };
-
-  const getDetectionsIndexForTime = (time) => {
-    if (!detectionTimes) {
-      return 0;
-    }
-    return Math.max(findInsertionPoint(detectionTimes, time) - 1, 0);
   };
 
   ///////////////
@@ -83,17 +62,6 @@ const VideoDetect = () => {
   ///////////
   const onTrackingRequestClick = () => {
     mutation.mutate({ url: videoLink, start_seconds: startTime, duration: duration, setup_id: setupId });
-  };
-
-  const selectImage = () => {
-    if (detectionTimes && detections) {
-      const timeIndex = Math.max(findInsertionPoint(detectionTimes, sliderTime) - 1, 0);
-      if (timeIndex >= 0 && timeIndex < detections.length) {
-        return detections[timeIndex].images[selectedCameraView];
-      }
-    }
-
-    return "";
   };
 
   return (
@@ -166,67 +134,7 @@ const VideoDetect = () => {
         </VStack>
       </VStack>
 
-      <VStack>
-        <Heading as="h3" size="md">
-          Camera Views
-        </Heading>
-
-        <Select.Root
-          value={selectedCameraView ? [selectedCameraView] : []}
-          collection={createListCollection({
-            items: detections ? Object.keys(detections[getDetectionsIndexForTime(sliderTime)].images) : [],
-          })}
-          size="sm"
-          width="320px"
-          onValueChange={(details) => setSelectedCameraView(details.value[0])}
-        >
-          <Select.Control>
-            <Select.Trigger>
-              <Select.ValueText placeholder="Select Camera View" />
-            </Select.Trigger>
-          </Select.Control>
-          <Portal>
-            <Select.Positioner>
-              <Select.Content>
-                {detections &&
-                  Object.keys(detections[getDetectionsIndexForTime(sliderTime)].images).map((key) => (
-                    <Select.Item item={key} key={key}>
-                      {key}
-                      <Select.ItemIndicator />
-                    </Select.Item>
-                  ))}
-              </Select.Content>
-            </Select.Positioner>
-          </Portal>
-        </Select.Root>
-
-        <Box position="relative">
-          {detections &&
-            imageDimensions &&
-            selectedCameraView &&
-            detections[getDetectionsIndexForTime(sliderTime)].detections[selectedCameraView].map((detection) => (
-              <Box
-                position="absolute"
-                left={toIntPercent(detection.image_coordinates[0], imageDimensions.width) + "%"}
-                top={toIntPercent(detection.image_coordinates[1], imageDimensions.height) + "%"}
-                width={toIntPercent(detection.image_coordinates[2], imageDimensions.width) + "%"}
-                height={toIntPercent(detection.image_coordinates[3], imageDimensions.height) + "%"}
-                border={"1px solid " + detection.color}
-                opacity="1"
-                pointerEvents="none"
-                zLevel="1000"
-              ></Box>
-            ))}
-          {detections && (
-            <ImageViewer
-              file={selectImage()}
-              includeLoadButton={false}
-              onImageDimensionChange={setImageDimensions}
-              encodingType="data:image/png;base64,"
-            ></ImageViewer>
-          )}
-        </Box>
-      </VStack>
+      <DetectionViewer selectedTime={sliderTime} detections={detections} detectionTimes={detectionTimes} />
     </HStack>
   );
 };
